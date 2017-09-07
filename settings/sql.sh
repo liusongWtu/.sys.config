51080a207d37250740c4dee30fb4424e51e5c8a7
liusongWtu <997896026@qq.com>
2017-09-07 17:00:25 +0800

    增加SVN便捷命令，启动数据库命令

---
+++ settings/sql.sh
@@ -0,0 +1,3 @@
+alias mysqlstart='sudo /usr/local/mysql/support-files/mysql.server start'
+alias mysqlstop='sudo /usr/local/mysql/support-files/mysql.server stop'
+alias mysqlrestart='sudo /usr/local/mysql/support-files/mysql.server restart'
